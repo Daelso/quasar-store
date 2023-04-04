82bc34533b535a20c09f38f6c59ca5c6a3d81c45
Chase Murtaugh <chasemurtaugh@gmail.com>
2023-04-04 01:00:49 -0400

    Design image upload

---
+++ src/pages/admin/createEditDesign.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <Suspense>
+      <createDesigns />
+    </Suspense>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import createDesigns from "src/components/admin/createEditDesign";
+
+export default defineComponent({
+  name: "create-edit-designs",
+  components: { createDesigns },
+});
+</script>
